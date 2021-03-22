6beb750f795dc764c7a9222931fb145f7140faa6
katefrolova <35382805+katefrolova@users.noreply.github.com>
2021-03-23 02:42:18 +0300

    Update 08_task_func.py

--- Module4/practice/funcs_part1/08_task_func.py
+++ Module4/practice/funcs_part1/08_task_func.py
@@ -1,1 +1,13 @@
 # Напишите функцию находящую n-ое число Фибоначчи.
+def fib_num(n):
+    fib0 = 0
+    fib1 = fib2 = 1
+    i = 0
+    while i < n - 3:
+        fib = fib0 + fib1 + fib2
+        fib1 = fib2
+        fib2 = fib
+        i += 1
+    return (fib)
+
+print(fib_num(7))

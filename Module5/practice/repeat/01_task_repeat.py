b2e1a29dff1e7565afe39d9ce7e4a5aaf1d40609
katefrolova <35382805+katefrolova@users.noreply.github.com>
2021-03-23 10:45:08 +0300

    Update 01_task_repeat.py

--- Module5/practice/repeat/01_task_repeat.py
+++ Module5/practice/repeat/01_task_repeat.py
@@ -2,6 +2,15 @@
 # произвольными целыми числами в заданном диапазоне.
 # , где size - размер генерируемого списка c элементами в диапазоне от of до to.
 
-def gen_list(size, of, to):
-    pass
+import random
 
+def gen_list(size, of, to):
+    list = []
+    i = 0
+    while i <= size:
+        element = random.randrange(of, to)
+        list.append(element)
+        i += 1
+    return list
+
+print(gen_list(10, 0, 100))

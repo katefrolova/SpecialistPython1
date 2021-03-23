b603d5050d016a8998d3c67ea48fdbb55f42bd71
katefrolova <35382805+katefrolova@users.noreply.github.com>
2021-03-24 01:10:09 +0300

    Update 04_task_while.py

--- Module2/practice/while/04_task_while.py
+++ Module2/practice/while/04_task_while.py
@@ -7,3 +7,10 @@
 # Формат выходных данных: Необходимо вывести сумму чисел написанных на кирпичах.
 
 # TODO: your code here
+n = int(input("input number:"))
+i = 1
+sum = 0
+while i <= n:
+    sum = sum + i ** 2
+    i +=1
+print(sum)

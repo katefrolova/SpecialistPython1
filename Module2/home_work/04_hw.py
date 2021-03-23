5880a6ab02a6a608662aea8236334c38f2306ec9
katefrolova <35382805+katefrolova@users.noreply.github.com>
2021-03-24 00:23:01 +0300

    Update 04_hw.py

--- Module2/home_work/04_hw.py
+++ Module2/home_work/04_hw.py
@@ -14,3 +14,10 @@
 #    #
 #    #
 ######
+
+a = int(input("a="))
+for i in range(a):
+    if i == 0 or i == a - 1:
+            print(a * "#")
+    else:
+        print("#", (a - 2) * ' ', '#', sep='')

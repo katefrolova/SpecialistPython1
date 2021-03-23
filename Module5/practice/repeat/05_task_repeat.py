a7b6aac55eed42a4069d8c914f719763cc21a078
katefrolova <35382805+katefrolova@users.noreply.github.com>
2021-03-23 23:13:16 +0300

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,10 @@
 # Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
 # Вычислите, сколько объектов будет отображаться на последней странице сайта.
 
+def items_on_last_page(num_items, items_on_page):
+    if num_items % items_on_page == 0:
+        return items_on_page
+    else:
+        return num_items % items_on_page
+
+print(items_on_last_page(8, 4))

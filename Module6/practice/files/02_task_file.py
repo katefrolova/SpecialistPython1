d86cac8f4722d17f0b5fa6e4b64469805ec9693b
katefrolova <35382805+katefrolova@users.noreply.github.com>
2021-03-24 12:05:22 +0300

    Update 02_task_file.py

--- Module6/practice/files/02_task_file.py
+++ Module6/practice/files/02_task_file.py
@@ -4,3 +4,18 @@
 # 1. На какую сумму было продано товаров
 # 2. Цену самого дорогого товара
 # 3. Цену самого дешевого товара
+f = open("data1/my_file.txt", "r", encoding="utf-8")
+costs_str = []
+for line in f:
+    costs_str += line.split()
+costs = []
+for el in costs_str:
+    costs.append(float(el))
+for el in costs:
+    mincost = min(costs)
+    maxcost = max(costs)
+
+print(costs)
+print(mincost)
+print(maxcost)
+f.close()

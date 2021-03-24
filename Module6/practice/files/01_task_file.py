2461035139903c6f5964487f49b6f31f0f8f13cc
katefrolova <35382805+katefrolova@users.noreply.github.com>
2021-03-24 11:28:10 +0300

    Update 01_task_file.py

--- Module6/practice/files/01_task_file.py
+++ Module6/practice/files/01_task_file.py
@@ -4,3 +4,19 @@
 # 2. Узнайте количество непробельный символов в данном файле
 # 3. Узнайте количество стихотворений, если известно,
 # что каждое стихотворение отделяется пустой строкой от предыдущего
+f = open("data1/my_file.txt", "r", encoding="utf-8")
+#for line in f:
+#    print(line.rstrip())
+
+sum = 0
+lim= 0
+for line in f:
+    print(line.rstrip())
+    if line.strip() == "":
+        lim += 1
+    line = line.replace(" ", "")
+    line = line.replace("\n", "")
+    sum += len(line)
+
+print(sum)
+print(lim + 1)
